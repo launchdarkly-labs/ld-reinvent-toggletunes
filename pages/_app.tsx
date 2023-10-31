2000fe4b5f332ddb95e9b019702f41afa8a80a97
Cody De Arkland <codydearkland@gmail.com>
2023-10-31 16:26:10 -0700

    structure corrections

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,38 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import NoSSRWrapper from "@/components/nossr";
+import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 
 let c;
 
 if (typeof window !== "undefined") {
+  const uniqueKey = 1;
 
+  const LDProvider = await asyncWithLDProvider({
+    clientSideID: process.env.NEXT_PUBLIC_LD_CLIENT_KEY || "",
+    reactOptions: {
+      useCamelCaseFlagKeys: false
+    },
+    context: {
+      kind: "multi",
+      user: {
+        key: uniqueKey, 
+        name: "anonymous",
+        appName: "ToggleTunes"
+      },
+      device: {
+        key: uniqueKey,
+        operating_system: "MacOS",
+        mobile_device: "False",
+      },
+    },
+  });
   c = ({ Component, pageProps }: AppProps) => {
     return (
       <NoSSRWrapper>
+        <LDProvider>
         <Component {...pageProps} />
+        </LDProvider>
       </NoSSRWrapper>
     );
   };
@@ -18,4 +41,3 @@ if (typeof window !== "undefined") {
 }
 
 export default c;
-
