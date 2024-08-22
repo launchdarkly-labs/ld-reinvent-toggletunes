38f500d1d6ce191109b70c1ce168188262887e97
Tony <tony.w.trinh3@gmail.com>
2024-08-22 16:15:45 -0400

    made tracklist table look graeat

--- components/musicapp.tsx
+++ components/musicapp.tsx
@@ -10,6 +10,7 @@ import { useBroadcastEvent, useEventListener } from "../liveblocks.config";
 import { Room } from "./room";
 import SimplePlayerScreen from "./SimplePlayerScreen";
 import MusicPlayingBar from "./MusicPlayingBar";
+import { RxCounterClockwiseClock } from "react-icons/rx";
 
 //TODO: when you go into playlist 1 /2 or whatever, it should be specific per team1/ team 2 etc
 //TODO: i think release should be a really ugly version of spotify from 2012 and then release a new version
@@ -278,42 +279,56 @@ export default function MusicApp({ teamName }: { teamName: string }) {
                         ))}
                       </div>
                     ) : (
-                      <table
-                        className="relative flex flex-col w-full overflow-y-auto "
-                        id="songs-bulleted-list"
-                      >
-                        {songsAPI.map((song: any, index: number) => (
-                          <motion.tr
-                            initial={{ opacity: 0, scale: 0.25 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            transition={{
-                              duration: 0.5,
-                              ease: [0, 0.71, 0.2, 1.01],
-                              delay: index * 0.2,
-                            }}
-                            key={song.id}
-                            className="grid grid-cols-3 items-center w-full text-lg border-b-2 border-b-gray-600/40 py-3 hover:bg-gray-500/20"
+                      <table id="songs-bulleted-list" className="mx-4">
+                        <thead>
+                          <tr
+                            className="grid grid-cols-[minmax(0,_0.1fr)_minmax(0,_1.5fr)__minmax(0,_1fr)_minmax(0,_0.1fr)] items-center text-lg
+                             border-b-2 border-b-gray-600/40 p-3 justify-items-start"
                           >
-                            <td className="flex gap-x-4 items-center">
-                              {newtoggledb !== "complete" ? (
-                                <Music2Icon className="h-10 w-10" />
-                              ) : (
-                                <img src={song.image} className="h-10 w-10" />
-                              )}
-                              <span className="flex flex-col">
-                                <p>{song.title}</p>
-                                <p className="text-base text-gray-500">{song.artists}</p>
-                              </span>
-                            </td>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Album</th>
+                            <th>
+                              <RxCounterClockwiseClock className="h-5 w-5" />
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className=" ">
+                          {songsAPI.map((song: any, index: number) => (
+                            <motion.tr
+                              initial={{ opacity: 0, scale: 0.25 }}
+                              animate={{ opacity: 1, scale: 1 }}
+                              transition={{
+                                duration: 0.5,
+                                ease: [0, 0.71, 0.2, 1.01],
+                                delay: index * 0.2,
+                              }}
+                              key={song.id}
+                              className="grid grid-cols-[minmax(0,_0.1fr)_minmax(0,_1.5fr)__minmax(0,_1fr)_minmax(0,_0.1fr)] items-center  text-lg
+                             border-b-2 border-b-gray-600/40 p-3 hover:bg-gray-500/20"
+                            >
+                              <td className="">{index + 1}</td>
+                              <td className="flex gap-x-4 items-center">
+                                {newtoggledb !== "complete" ? (
+                                  <Music2Icon className="h-10 w-10" />
+                                ) : (
+                                  <img src={song.image} className="h-10 w-10" />
+                                )}
+                                <span className="flex flex-col">
+                                  <p>{song.title}</p>
+                                  <p className="text-base text-gray-500">{song.artists}</p>
+                                </span>
+                              </td>
 
-                            <td className="">
-                              <p>{song.album}</p>
-                            </td>
-                            <td className=" text-gray-500 ">
-                              <p>{song.duration}</p>
-                            </td>
-                          </motion.tr>
-                        ))}
+                              <td className="">
+                                <p>{song.album}</p>
+                              </td>
+                              <td className=" text-gray-500 ">
+                                <p>{song.duration}</p>
+                              </td>
+                            </motion.tr>
+                          ))}
+                        </tbody>
                       </table>
                     )}
                   </section>
