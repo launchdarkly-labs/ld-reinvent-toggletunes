0499d49c81f65addfcf8973890b2542bc57ace51
Tony <tony.w.trinh3@gmail.com>
2024-09-24 23:18:56 -0400

    got it to work

--- pages/api/apiReset.ts
+++ pages/api/apiReset.ts
@@ -4,39 +4,32 @@ import type { NextApiRequest, NextApiResponse } from "next";
 // this prevents us from accidentally deleting flags from the wrong project
 // since the API key has access to all projects
 //"toggle-tunes-team-1"
-const projectKeys: Array<string> = [
-  "team-1",
-  "team-2",
-  "team-3",
-  "team-4",
-];
+const projectKeys: Array<string> = ["team-1", "team-2", "team-3", "team-4"];
 const API_KEY: string = process.env.LD_API_KEY as string;
 const delay = 1000;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   console.log("Reset starting");
   try {
     for (const projectKey of projectKeys) {
       // get the flags then delete them
       const flags = await getFlags(projectKey);
-      console.log("flags",flags)
+      console.log("flags", flags);
       for (const flag of flags.items) {
+        console.log("flag", flag);
         await deleteFlag(projectKey, flag.key);
         await sleep(delay);
       }
       // get environments which we need for deleting segments
-      const environments = await getEnvironments(projectKey);
-      // get the segments for each environment and delete them
-      for (const environment of environments.items) {
-        const segments = await getSegments(projectKey, environment.key);
-        for (const segment of segments.items) {
-          await deleteSegment(projectKey, environment.key, segment.key);
-          await sleep(delay);
-        }
-      }
+      // const environments = await getEnvironments(projectKey);
+      // // get the segments for each environment and delete them
+      // for (const environment of environments.items) {
+      //   const segments = await getSegments(projectKey, environment.key);
+      //   for (const segment of segments.items) {
+      //     await deleteSegment(projectKey, environment.key, segment.key);
+      //     await sleep(delay);
+      //   }
+      // }
     }
   } catch (e: any) {
     return res.status(500).json({ error: e.message });
@@ -68,8 +61,10 @@ async function getEnvironments(projectKey: string) {
   return data;
 }
 
+//09-23-2024 this works now
 async function getFlags(projectKey: string) {
   //TODO: NEED TO CHANGE THIS TO SEPARATE PROJECTS
+  //   `https://app.launchdarkly.com/api/v2/flags/${projectKey}`,
   const resp = await fetch(
     `https://app.launchdarkly.com/api/v2/flags/toggletunes?env=${projectKey}&selected-env=${projectKey}`,
     {
@@ -99,48 +94,51 @@ async function deleteFlag(projectKey: string, flagKey: string) {
   }
 
   console.log("Running the disable function for the flag " + flagKey);
+  //    `https://app.launchdarkly.com/api/v2/flags/${projectKey}/${flagKey}`,
+  //`https://app.launchdarkly.com/api/v2/flags/toggletunes/${flagKey}/targeting?env=${projectKey}`,
   const disableResp = await fetch(
-    `https://app.launchdarkly.com/api/v2/flags/${projectKey}/${flagKey}`,
+    `https://app.launchdarkly.com/api/v2/flags/toggletunes/${flagKey}`,
     {
       method: "PATCH",
       headers: {
         Authorization: API_KEY,
-        "Content-Type":
-          "application/json; domain-model=launchdarkly.semanticpatch",
+        "Content-Type": "application/json; domain-model=launchdarkly.semanticpatch",
       },
       body: JSON.stringify({
-        environmentKey: "test",
+        environmentKey: projectKey,
         instructions: [{ kind: "turnFlagOff" }],
       }),
     }
   );
-
+  console.log("disableResp", disableResp);
   if (!disableResp.ok) {
-    throw new Error(
-      `Cannot disable flag ${flagKey}: ${disableResp.statusText}`
-    );
+    throw new Error(`Cannot disable flag ${flagKey}: ${disableResp.statusText}`);
   }
 
   console.log("Running the delete function for the flag " + flagKey);
-  const deleteResp = await fetch(
-    `https://app.launchdarkly.com/api/v2/flags/${projectKey}/${flagKey}`,
-    {
-      method: "DELETE",
-      headers: {
-        Authorization: API_KEY,
-      },
-    }
-  );
-
-  let data;
-  if (deleteResp.ok) {
-    data = await deleteResp.text();
-    if (data) {
-      data = JSON.parse(data);
-    }
-  } else {
-    throw new Error(`Cannot delete flag ${flagKey}: ${data ?? "unknown"}`);
-  }
+  // `https://app.launchdarkly.com/api/v2/flags/${projectKey}/${flagKey}`,
+  // const deleteResp = await fetch(
+  //   `https://app.launchdarkly.com/api/v2/flags/${projectKey}/${flagKey}`,
+  //   {
+  //     method: "DELETE",
+  //     headers: {
+  //       Authorization: API_KEY,
+  //     },
+  // body: JSON.stringify({
+  //   environmentKey: projectKey,
+  // }),
+  //   }
+  // );
+  // console.log("deleteResp", deleteResp);
+  // let data;
+  // if (deleteResp.ok) {
+  //   data = await deleteResp.text();
+  //   if (data) {
+  //     data = JSON.parse(data);
+  //   }
+  // } else {
+  //   throw new Error(`Cannot delete flag ${flagKey}: ${data ?? "unknown"}`);
+  // }
 }
 
 async function getSegments(projectKey: string, environmentKey: string) {
@@ -167,11 +165,7 @@ async function getSegments(projectKey: string, environmentKey: string) {
   return data;
 }
 
-async function deleteSegment(
-  projectKey: string,
-  environmentKey: string,
-  segmentKey: string
-) {
+async function deleteSegment(projectKey: string, environmentKey: string, segmentKey: string) {
   if (!projectKeys.includes(projectKey)) {
     throw new Error("Cannot delete segments from an unspecified project");
   }
@@ -192,9 +186,7 @@ async function deleteSegment(
       data = JSON.parse(data);
     }
   } else {
-    throw new Error(
-      `Cannot delete segment ${segmentKey}: ${data ?? "unknown"}`
-    );
+    throw new Error(`Cannot delete segment ${segmentKey}: ${data ?? "unknown"}`);
   }
 }
 
