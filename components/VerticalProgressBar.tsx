9f3ffeae67ad165516a90f8228cee84bf4971e1c
Tony <tony.w.trinh3@gmail.com>
2024-11-26 14:37:27 -0500

    vertical bar

--- components/VerticalProgressBar.tsx
+++ components/VerticalProgressBar.tsx
@@ -1,45 +1,44 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Button } from "@/components/ui/button";
 
+const VerticalProgressBar = ({
+  progressBarHeight,
+  colorProgress,
+  barColor,
+  color,
+}: {
+  progressBarHeight: number;
+  colorProgress: number;
+  barColor: string;
+  color: string;
+}) => {
 
-
-const VerticalProgressBar= ({progressBarHeight, colorProgress, barColor, color}: {progressBarHeight:number,colorProgress:number, barColor:string, color:string}) => {
-  const initialProgress = 0;
-  const [progress, setProgress] = useState(initialProgress);
-
-  const incrementProgress = () => {
-    setProgress((prevProgress) => Math.min(prevProgress + 5, 100));
-  };
 
   const segments = 20;
   const gap = 10; // Gap between segments in pixels
   const segmentHeight = Math.ceil(progressBarHeight / segments);
-  console.log(segmentHeight);
   const filledSegments = (colorProgress: number) => Math.ceil((colorProgress / 100) * segments); //convert from 60% to 12 /20 segment
 
   return (
     <div
-            className="w-[20%] bg-transparent rounded-none overflow-hidden relative flex flex-col-reverse justify-start items-center"
-            style={{ height: `${progressBarHeight}px` }}
-            id={`progress-bar-${color}`}
-          >
-            {Array.from({ length: segments }).map((_, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`w-full rounded-none transition-all duration-300 ease-out ${
-                    index < filledSegments(colorProgress)
-                      ? barColor
-                      : "bg-zinc-500/20"
-                  }`}
-                  style={{
-                    height: `${segmentHeight - gap}px`,
-                    marginBottom: `${index == 0 ? 30 : gap}px`,
-                  }}
-                />
-              );
-            })}
-          </div>
+      className="w-[20%] bg-transparent rounded-none overflow-hidden relative flex flex-col-reverse justify-start items-center"
+      style={{ height: `${progressBarHeight}px` }}
+      id={`progress-bar-${color}`}
+    >
+      {Array.from({ length: segments }).map((_, index) => {
+        return (
+          <div
+            key={index}
+            className={`w-full rounded-none transition-all duration-300 ease-out ${
+              index < filledSegments(colorProgress) ? barColor : "bg-zinc-500/20"
+            }`}
+            style={{
+              height: `${segmentHeight - gap}px`,
+              marginBottom: `${index == 0 ? 30 : gap}px`,
+            }}
+          />
+        );
+      })}
+    </div>
   );
 };
 
--- components/progress-screen.tsx
+++ components/progress-screen.tsx
@@ -31,7 +31,7 @@ export function ProgressStatus({
       <section
         id="scoreboard-body"
         className=" bg-[url('/images/scoreboard-background-no-bg-line.svg')]
-     bg-cover bg-no-repeat grid- h-full w-full  border-b-[1px] border-zinc-500 z-10"
+     bg-contain bg-no-repeat grid- h-full w-full  border-b-[1px] border-zinc-500 z-10"
         ref={progresBarHeightRef}
       >
         <div className="flex justify-evenly items-end  w-full h-full mx-auto">
