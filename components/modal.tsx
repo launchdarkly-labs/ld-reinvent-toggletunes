3cec5597c5db4fd4f1bab70dd4ba77c99c805da1
Tony <tony.w.trinh3@gmail.com>
2024-12-01 23:07:58 -0800

    tried ot make it click to close if they want to look at the project afte

--- components/modal.tsx
+++ components/modal.tsx
@@ -6,23 +6,36 @@ import {
   AlertDialogHeader,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PURPLE, RED, BLUE, GREEN } from "@/lib/constant";
 
-export function Modal({ winnerName, setOpenStartModal }: {winnerName: string, setOpenStartModal?: any}) {
+export function Modal({
+  winnerName,
+  setOpenStartModal,
+}: {
+  winnerName: string;
+  setOpenStartModal?: any;
+}) {
+  const [isOpen, setIsOpen] = useState(true);
   useEffect(() => {
     //hide the starting modal when you trigger manually the color winner
     if (winnerName !== "" && setOpenStartModal) {
       setOpenStartModal(false);
+      setIsOpen(true);
     }
   }, [winnerName]);
 
   // they overwrote alert dialog with the winner gif
   return (
     <>
-      <AlertDialog open={true}>
-        <AlertDialogTrigger>
-          <AlertDialogContent variant={winnerName}>
+      <AlertDialog open={isOpen}>
+        <AlertDialogTrigger
+          asChild
+          // onClick={() => {
+          //   setIsOpen(false);
+          // }}
+        >
+          <AlertDialogContent variant={winnerName} >
             <AlertDialogHeader />
             <AlertDialogFooter></AlertDialogFooter>
           </AlertDialogContent>
